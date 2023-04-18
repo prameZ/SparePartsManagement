2b3874d88e8f1ceb06d6663360a026e156e2d820
PRAME-PC\Prame <pram44@hotmail.co.th>
2023-04-19 03:29:18 +0700

    first commit

--- pages/employees/new/index.js
+++ pages/employees/new/index.js
@@ -159,7 +159,7 @@ const CreateDataOfTheEmployees = () => {
     } else if (AlartUsername === false) {
     } else {
       try {
-        await axios.post("https://blush-seahorse-boot.cyclic.app/addEmployees", DataEmployees);
+        await axios.post("https://blush-seahorse-boot.cyclic.app/addEmployees", Name);
         console.log("SaveData to Employees Success");
         router.push("/employees/");
         setAddEmpSucces(true)
